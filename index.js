12f1f1dcb13e510a2092a6309bd4031f4f81b97a
jxszlh <jxszlh@gmail.com>
2018-03-08 22:24:05 +0800

    fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const waitLogin = page => {
       const int = setInterval(async () => {
         const el = await page.$('#safe-logout')
         if(el){
-          console.log('find safe-logout', int)
+          //console.log('find safe-logout', int)
           clearInterval(int)
           res()
         }
@@ -21,16 +21,43 @@ const waitLogin = page => {
 (async () => {
   const browser = await puppeteer.launch({headless:false})
   const page = await browser.newPage()
-  await page.goto('https://user.lu.com/user/login?returnPostURL=https://www.lu.com')
+  //await page.goto('https://user.lu.com/user/login?returnPostURL=https://www.lu.com')
 
-  await waitLogin(page)
+  //await waitLogin(page)
 
-  console.log('logged on .............')
+  //console.log('logged on .............')
 
   await page.goto('https://list.lu.com/list/transfer-p2p')
 
   console.log('go to p2p......')
 
-  await browser.close()
+  let invested = false
+
+  while (!invested) {
+    await page.goto('https://list.lu.com/list/transfer-p2p?notHasBuyFeeRate=true')
+    const assets = await page.evaluate(() => {
+      return Array.prototype.slice.apply(document.querySelectorAll('.product-list'))
+        .map(item => {
+          const amount = parseFloat(item.querySelector('.product-amount em').innerText.replace(',', ''))
+          const url = item.querySelector('.product-status a').href
+          return {
+            amount,
+            url,
+          }
+        })
+        .filter(({amount}) => amount > 5000 && amount < 6500)
+    })
+
+    console.log(assets)
+
+
+    await page.waitFor(1000)
+  }
+
+//btns btn_xlarge investBtn sk-area-trigger
+
+
+
+  //await browser.close()
 
 })();
